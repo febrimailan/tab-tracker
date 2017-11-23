220141511f009847ed548710f8b0392f532617f8
febrimailand <febri.mailand@gmail.com>
2017-11-23 19:47:15 +0700

    Sequelize

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
-    <h1>Register</h1>
-
-    <input type="email" name="email" v-model="email" placeholder="email" />
-    <br> <br>
-    <input type="password" name="password" v-model="password" placeholder="password" />
-    <br> <br>
-    <button @click="register">Register</button>
-  </div>
-
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input class="blue lighten-4" type="email" name="email" v-model="email" placeholder="email" />
+          <br>
+          <br>
+          <input class="blue lighten-4" type="password" name="password" v-model="password" placeholder="password" />
+          <br>
+          <br>
+          <v-btn class="cyan" @click="register">Register</v-btn>
+          <div class="error" v-html="error" />
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -17,7 +26,8 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   watch: {
@@ -27,21 +37,28 @@ export default {
   },
   methods: {
     async register () {
-      // console.log('register button was click', this.email, this.password)
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        // console.log('register button was click', this.email, this.password)
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+      
     }
   },
   mounted () {
-    setTimeout(() => {
-      this.email = 'Hello World'
-    }, 2000)
+    // setTimeout(() => {
+    //   this.email = 'Hello World'
+    // }, 2000)
   }
 }
 </script>
 
 <style scoped>
-
+  .error {
+    color: red;
+  }
 </style>
