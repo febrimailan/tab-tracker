fa344f7d2f696883b54c588db95eed75c9a30cfb
febrimailand <febri.mailand@gmail.com>
2017-11-26 22:52:26 +0700

    login

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -3,25 +3,20 @@
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>
+          <v-toolbar-title>Login</v-toolbar-title>
         </v-toolbar>
         <div class="pl-4 pr-4 pt-2 pb-2">
-          <form 
-            name="tab-tracker-form"
-            autocomplete="off">
-            <v-text-field
-                  label="Email"
-                  v-model="email"            
-                ></v-text-field>
-                <v-text-field             
-                  label="Password"
-                  type="password"            
-                  v-model="password"
-                  autocomplete="new-password"
-                ></v-text-field>
-          </form>
+         <v-text-field
+              label="Email"
+              v-model="email"            
+            ></v-text-field>
+            <v-text-field             
+              label="Password"
+              type="password"
+              v-model="password"
+            ></v-text-field>
           <br>
-          <v-btn dark class="cyan" @click="register">Register</v-btn>
+          <v-btn dark class="cyan" @click="login">Login</v-btn>
           <div class="error" v-html="error" />
         </div>
       </div>
@@ -40,9 +35,9 @@ export default {
     }
   },
   methods: {
-    async register () {
+    async login () {
       try {
-        const response = await AuthenticationService.register({
+        const response = await AuthenticationService.login({
           email: this.email,
           password: this.password
         })
